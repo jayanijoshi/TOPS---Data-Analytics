6eb1bc60ef49a6a6e21c788c4b3310c69d09bca8
Jayani Joshi <jayani2canada@gmail.com>
2025-01-29 15:19:31 +0530

    Assesment_Q1.sql
    Correctly formatted file

--- Assesment_Q1.sql
+++ Assesment_Q1.sql
@@ -26,37 +26,37 @@
         
 			/* Using Order By Clause */
         select * from assessment_1_Q1 order by first_name;  -- Sorting worker details by first_name ascending --
-		select * from assessment_1_Q1 order by department desc;  -- Sorting worker details by department descending --
+	select * from assessment_1_Q1 order by department desc;  -- Sorting worker details by department descending --
 		
 -- B : Write an SQL query to print details for Workers with the first names “Vipul” and “Satish” from the Worker table.--
 			
-            /* Using IN Clause */
+        		/* Using IN Clause */
         select * from assessment_1_Q1 where first_name in ('Vipul', 'Satish');
         
 -- C : Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘h’ and contains six alphabets. --
-		   /* Using " %_% " filter */
-		select * from assessment_1_Q1 where first_name like '_____h';
+			/* Using " %_% " filter */
+	select * from assessment_1_Q1 where first_name like '_____h';
         
 -- D : Write an SQL query to print details of the Workers whose SALARY lies between 50000 to 250000. --
-		   /* Using "AND" clause */
-		select * from assessment_1_Q1 where salary>=50000 and salary<=250000;
+			/* Using "AND" clause */
+	select * from assessment_1_Q1 where salary>=50000 and salary<=250000;
 		
 -- E : Write an SQL query to fetch duplicate records having matching data in some fields of a table. --
-		   /* Using "GROUP BY" and "HAVING" */
-select worker_id,first_name,last_name,salary,joining_date,department, COUNT(*) as duplicates from assessment_1_q1 group by worker_id,first_name,last_name,salary,joining_date,department having COUNT(*) > 1;
+			/* Using "GROUP BY" and "HAVING" */
+	select worker_id,first_name,last_name,salary,joining_date,department, COUNT(*) as duplicates from assessment_1_q1 group by worker_id,first_name,last_name,salary,joining_date,department having COUNT(*) > 1;
         
 -- F : Write an SQL query to show the top 6 records of a table. --
-		   /* Using "limit" clause */
-		select * from assessment_1_Q1 limit 6;
+			/* Using "limit" clause */
+	select * from assessment_1_Q1 limit 6;
         
 -- G : Write an SQL query to fetch the departments that have less than five people in them. --
-		   /* Using "GORUP BY" , "COUNT" & "HAVING" clause */
-		select department from assessment_1_Q1 group by department having count(*) < 5;
+			/* Using "GORUP BY" , "COUNT" & "HAVING" clause */
+	select department from assessment_1_Q1 group by department having count(*) < 5;
         
 -- H : Write an SQL query to show all departments along with the number of people in there. --
-		   /* Using "GORUP BY" , "COUNT" & "HAVING" clause */
-		select department, count(*) as Total_staff from assessment_1_q1 group by department;
+			/* Using "GORUP BY" , "COUNT" & "HAVING" clause */
+	select department, count(*) as Total_staff from assessment_1_q1 group by department;
 
 -- I : Write an SQL query to print the name of employees having the highest salary in each department. --
-		   /* Using "GORUP BY" & "HAVING" clause */
-		select department,max(salary) from assessment_1_q1 group by department having max(salary);
+			/* Using "GORUP BY" & "HAVING" clause */
+	select department,max(salary) from assessment_1_q1 group by department having max(salary);
